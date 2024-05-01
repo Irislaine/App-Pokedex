8eeb2d01f4d1446ee87b81c9d03729a1b15f7495
Irislaine <irislaineribeiro73@gmail.com>
2024-04-30 19:10:50 -0500

    card styles

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -23,53 +23,59 @@ function PokeCard({ url }) {
   }
 
   return (
-    <>
-      <article 
-        onClick={handlePokemon}
-        className={`pokecard pokecard--${pokemon?.types[0].type.name}`}
-      >
-        <div className={`pokecard_back ${pokemon?.types[0].type.name}`}></div>
+    <article 
+      onClick={handlePokemon}
+      className={`pokecard pokecard--${pokemon?.types[0].type.name}`}
+    >
+      <div className={`pokecard_back ${pokemon?.types[0].type.name}`}></div>
 
-        <figure className='pokecard_img'>
-          <img
-            src={pokemon?.sprites.other['official-artwork'].front_default}
-            alt="pokemon image"
-          />
-        </figure>
+      <figure className='pokecard_img'>
+        <img
+          src={pokemon?.sprites.other['official-artwork'].front_default}
+          alt="pokemon image"
+        />
+      </figure>
 
-        <div className='pokecard_info'>
-          <h3 className={`stats--${pokemon?.types[0].type.name}`} >
-            {capitalizeFirstLetter(pokemon?.name)}
-          </h3>
-        </div>
+      <div className='pokecard_info'>
+        <h3 className={`stats--${pokemon?.types[0].type.name}`} >
+          {capitalizeFirstLetter(pokemon?.name)}
+        </h3>
+      </div>
 
-        <ul className='pokecard_types'>
-          {
-            pokemon?.types.map(type => (
-              <li
-                className={`slot${type.slot}`}
-                key={type.type.url}
-              >
-                {capitalizeFirstLetter(type.type.name)}
-              </li>
-            ))
-          }
-        </ul>
+      <ul className='pokecard_types'>
+        {
+          pokemon?.types.map(type => (
+            <li
+              className={`slot${type.slot}`}
+              key={type.type.url}
+            >
+              {capitalizeFirstLetter(type.type.name)}
+            </li>
+          ))
+        }
+      </ul>
 
-        <span>Type:</span>
+      <span className='pokecard_type'>Type</span>
 
-        <hr />
+      <hr />
 
-        <ul className='pokecard_stats'>
-          {
-            pokemon?.stats.map(stat => (
-              !stat.stat.name.includes('-') &&
-              <li key={stat.stat.url}><span className={`stats--${pokemon?.types[0].type.name}`}>{stat.stat.name.toUpperCase()}<span></span>{stat.base_stat}</span></li>
-            ))
-          }
-        </ul>
-      </article>
-    </>
+      <ul className='pokecard_stats'>
+        {
+          pokemon?.stats.map(stat => (
+            !stat.stat.name.includes('-') &&
+            <li key={stat.stat.url}>
+              <span className='pokecard_stat_name'>     
+                {stat.stat.name.toUpperCase()}
+              </span>
+            
+              <span className={`pokecard_stat_value stats--${pokemon?.types[0].type.name}`}>
+                {stat.base_stat}
+              </span>
+            </li>
+          ))
+        }
+      </ul>
+    </article>
   )
 }
 
